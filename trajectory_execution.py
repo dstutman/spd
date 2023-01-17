4c18d2b447cba7c6435fdf18694c94d896d59225
dstutman <git@stutman.tech>
2023-01-17 17:32:14 +0100

    Progress

--- trajectory_execution.py
+++ trajectory_execution.py
@@ -3,6 +3,8 @@ from frankx import Robot, LinearMotion, Affine
 robot = Robot("172.16.0.2")
 robot.recover_from_errors()
 
-robot.set_dynamic_rel(0.05)
+robot.set_dynamic_rel(0.1)
+robot.jerk_rel = 0.01
+robot.acceleration_rel = 0.01
 
 robot.move(LinearMotion(Affine(0.25, 0.25, 0.25)))
